51b857170eb7d7527914b4071cd6ffc965f5b517
Andreas Sandvik Høimyr <andrehoi@nmbu.no>
2019-10-21 11:46:52 +0200

    framework for classes ready.

--- src/andreas_hoimyr_ex/ex04/myrand.py
+++ src/andreas_hoimyr_ex/ex04/myrand.py
@@ -3,3 +3,22 @@
 
 __author__ = 'andreas sadnvik hoimyr'
 __email__ = 'andrehoi@nmbu.no'
+
+
+class LCGRand:
+
+    def __init__(self, seed):
+        pass
+
+    def rand(self):
+        pass
+
+
+class ListRand:
+
+    def __init__(self):
+        pass
+
+    def __rand__(self):
+        pass
+
