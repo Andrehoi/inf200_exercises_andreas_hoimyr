5bb2dc6f3244d6db8515cc83b3376063e88b67a6
Andreas Sandvik Høimyr <andrehoi@nmbu.no>
2019-10-23 10:38:14 +0200

    Further progress on class Walker

--- src/andreas_hoimyr_ex/ex04/walker.py
+++ src/andreas_hoimyr_ex/ex04/walker.py
@@ -4,26 +4,40 @@
 __author__ = 'andreas sandvik hoimyr'
 __email__ = 'andrehoi@nmbu.no'
 
+import random
+
 
 class Walker:
 
-    def __init__(self, samfundet_position, home_position):
-        self.student_position = samfundet_position
+    def __init__(self, samfunnet_position, home_position):
+        self.student = samfunnet_position
         self.home = home_position
-        pass
+        self.step = 0
+        self.count_steps = 0
+
+        return
 
     def move(self):
-        pass
+        self.step = random.randint(1, 2)
+        if self.step == 2:
+            self.step = -1
+        self.student += self.step
+
+        self.count_steps += 1
+
+        return
 
     def is_at_home(self):
-        pass
+        if self.student == self.home:
+            return True
+        else:
+            return False
 
     def get_position(self):
-        pass
+        return self.student
 
     def get_steps(self):
-        pass
+        return self.count_steps
 
-
-
-
+if __name__ == '__main__':
+    pass
