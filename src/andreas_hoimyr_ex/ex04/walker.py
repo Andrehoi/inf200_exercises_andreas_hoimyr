af7a7bbd7df576d23dca7ec1968d8eff0b88ff9f
Andreas Sandvik Høimyr <andrehoi@nmbu.no>
2019-10-24 12:23:14 +0200

    Both classes are completed, and pep8 errors fixed

--- src/andreas_hoimyr_ex/ex04/walker.py
+++ src/andreas_hoimyr_ex/ex04/walker.py
@@ -12,32 +12,54 @@ class Walker:
     def __init__(self, samfunnet_position, home_position):
         self.student = samfunnet_position
         self.home = home_position
-        self.step = 0
         self.count_steps = 0
 
         return
 
     def move(self):
-        self.step = random.randint(1, 2)
-        if self.step == 2:
-            self.step = -1
-        self.student += self.step
+        """ Moves the student either +1 step or -1 step towards home"""
+        step = random.randint(1, 2)
+        if step == 2:
+            step = -1
+        self.student += step
 
         self.count_steps += 1
 
         return
 
     def is_at_home(self):
+        """ Checks if student is at home """
         if self.student == self.home:
             return True
         else:
             return False
 
     def get_position(self):
+        """ Returns the position of the student"""
         return self.student
 
     def get_steps(self):
+        """Returns the number of steps to reach home"""
         return self.count_steps
 
+
 if __name__ == '__main__':
-    pass
+
+    progress_home = [1, 2, 5, 10, 20, 50, 100]
+
+    for distance in progress_home:
+
+        distance_moved = []
+
+        for _ in range(5):
+
+            walker = Walker(0, distance)
+
+            while not walker.is_at_home():
+
+                walker.move()
+
+            distance_moved.append(walker.get_steps())
+
+        print(' Distance:   {0} -> path lengths:    {1}'.format(
+            distance, distance_moved))
